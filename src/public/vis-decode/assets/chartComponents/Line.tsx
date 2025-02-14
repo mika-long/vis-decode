30b0295969aefd572b890cc56158e030e40f119a
Sheng Long <shenglong@u.northwestern.edu>
2025-02-14 17:52:00 -0600

    added content to Line.tsx

--- src/public/vis-decode/assets/chartComponents/Line.tsx
+++ src/public/vis-decode/assets/chartComponents/Line.tsx
@@ -1,1 +1,30 @@
-import * as d3 from "d3"; 
+import * as d3 from "d3";
+
+interface LineProps {
+	data: Array<{ x: number; y: number}>;
+	xScale: d3.ScaleLinear<number, number>;
+	yScale: d3.ScaleLinear<number, number>;
+	strokeColor?: string;
+	strokeWidth?: number;
+}
+
+export function Line({
+	data,
+	xScale,
+	yScale,
+	strokeColor = 'currentColor',
+	strokeWidth = 2,
+}: LineProps) {
+	const lineGenerator = d3.line<{x:number; y:number}>()
+		.x(d => xScale(d.x))
+		.y(d => yScale(d.y));
+
+	return (
+		<path
+			d = {lineGenerator(data) || ''}
+			fill="none"
+			stroke={strokeColor}
+			strokeWidth={strokeWidth}
+		/>
+	);
+}
