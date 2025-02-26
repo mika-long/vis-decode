51e6c8e1caa93b91a0065fd6149b917dda2dc173
Sheng Long <shenglong@u.northwestern.edu>
2025-02-26 17:45:25 -0600

    adding stuff related to jstate import types

---
+++ src/public/vis-decode-slider/assets/dataGeneration/jstat.d.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'jstat' {
+    // Define the main exports and types here
+    const jStat: any; // You can make this more specific later
+    export default jStat;
+}
